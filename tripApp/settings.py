c390d9ce99596b1961621f35bf832c414faadd66
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 13:34:26 +0300

    Switch SMTP port to 2525

--- tripApp/settings.py
+++ tripApp/settings.py
@@ -116,7 +116,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'b8e474a001@smtp-brevo.com')
