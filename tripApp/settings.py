e3ddd07f4ae608d7f281152de2176fb38f46489f
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 13:01:19 +0300

    Test email with console backend

--- tripApp/settings.py
+++ tripApp/settings.py
@@ -110,7 +110,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
