8aef5dc8c79eb9f276add069b39282d3a9727904
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 13:23:45 +0300

    Remove sensitive key and use env

--- tripApp/settings.py
+++ tripApp/settings.py
@@ -6,6 +6,10 @@ from pathlib import Path
 import os
 import dj_database_url
 
+import environ
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
