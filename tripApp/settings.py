4fbb6ccfd664dcb229004906fbe3cc47ecbefa8f
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 13:44:59 +0300

    changing email port

--- tripApp/settings.py
+++ tripApp/settings.py
@@ -116,9 +116,9 @@ LOGOUT_REDIRECT_URL = 'home'
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 2525
+EMAIL_PORT = 465
 EMAIL_USE_TLS = False
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'b8e474a001@smtp-brevo.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
