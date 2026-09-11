70fda2d2749adbd5f8056289c932bf816741cba2
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 13:41:32 +0300

    Disable TLS for port 2525 SMTP

--- tripApp/settings.py
+++ tripApp/settings.py
@@ -117,7 +117,8 @@ LOGOUT_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 2525
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'b8e474a001@smtp-brevo.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
