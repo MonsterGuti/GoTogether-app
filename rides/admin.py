df3129f51f54db773c55c6210f55d4574755c192
MonsterGuti <gogulanovmartin@gmail.com>
2026-09-11 12:45:20 +0300

    creating admin models

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ride, Booking, RideMessage, User
 
-# Register your models here.
+admin.site.register(Ride)
+admin.site.register(Booking)
+admin.site.register(RideMessage)
+admin.site.register(User)
